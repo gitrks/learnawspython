f5d15e54c8eaa17b679ec945893e1db05aaa0cbf
gitrks <ravishing.ravisingh@gmail.com>
2021-10-06 11:17:50 -0700

    With log as output

---
+++ lab_8_step_2_simple_logging.py
@@ -0,0 +1,35 @@
+# Import logging
+import logging
+import json
+
+# Set the log level in the basic configuration.  This means we will capture all our log entries and not just those at Warning or above.
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+# This uses a json string as an input
+json_string = """
+{
+    "Input":[
+        {
+        "Text":"I am learning to code in AWS",
+        "SourceLanguageCode":"en",
+        "TargetLanguageCode":"fr"
+        }
+    ]
+}
+"""
+
+json_input = json.loads(json_string)
+
+# Defines two variables to store the language code from the input.
+SourceLanguageCode = json_input['Input'][0]['SourceLanguageCode']
+TargetLanguageCode = json_input['Input'][0]['TargetLanguageCode']
+
+# The if statement checks to see if the language code is the same as the source code
+if SourceLanguageCode == TargetLanguageCode:
+    # This will print to the console as the default level is warning
+    logging.warning(
+        "The SourceLanguageCode is the same as the TargetLanguageCode - stopping")
+else:
+    # This will not print to the console because it is lower than warning
+    logging.info(
+        "The Source Language and Target Language codes are different - proceeding")
